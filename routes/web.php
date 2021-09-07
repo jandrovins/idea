3eac72aef989d775866c62fc8ce6824eb49f8055
Vincent A. Arcila <jandro.vins@gmail.com>
2021-09-07 14:45:34 +0000

    Updated source files to comply with code style

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name("home.index");
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 
 Auth::routes();
 
