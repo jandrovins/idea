b5ce2efd43111dd11ff9ca5c2e41e7437c302862
Vincent A. Arcila <jandro.vins@gmail.com>
2021-09-24 19:57:14 +0000

    Added method to manage top courses view

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,9 +1,11 @@
 <?php
 
+// Authors: Simón Flórez, Vincent A. Arcila
+// Last edition: September 24, 2021
+
 namespace App\Http\Controllers;
 
 use App\Models\Course;
-use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
@@ -27,5 +29,18 @@ class CourseController extends Controller
         ];
 
         return view('courses.show')->with('data', $data);
+    }
+
+    public function listTop()
+    {
+        // Select courses that have one or more reviews ; calculate their rating avg ; sort them by rating avg ; take top 3
+        $courses = Course::has('reviews')->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc')->take(3)->get();
+
+        $data = [
+            'title' => 'Our top rated courses',
+            'courses' => $courses,
+        ];
+
+        return view('courses.listTop')->with('data', $data);
     }
 }
