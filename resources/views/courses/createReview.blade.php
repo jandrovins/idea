f50436f34a2d7b94b397d34e93d2b0e9e786025f
Vincent A. Arcila <jandro.vins@gmail.com>
2021-09-25 15:55:02 +0000

    Move create reviews logic to independent view

---
+++ resources/views/courses/createReview.blade.php
@@ -0,0 +1,36 @@
+<!--Authors: Vincent Arcila (vaarcilal@eafit.edu.co)-->
+
+@if ( $data['isEnrolled'] )
+<div class="card">
+    <div class="card-header">@lang ("messages.course.createReview.cardTitle")</div>
+    <div class="card-body">
+        @if($errors->any())
+        <div class="alert alert-warning" role="alert">
+            <ul id="errors">
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <form method="POST" action="{{ route('review.save') }}">
+            @csrf
+            <div class="form-group">
+                <label for="rating">@lang ("messages.course.review.rating")</label>
+                <input type="number" class="form-control" name="rating" min="0" max="10" placeholder=10
+                    value="{{ old('title') }}">
+            </div>
+            <div class="form-group">
+                <label for="comment">@lang ("messages.course.review.comment")</label>
+                <textarea class="form-control" name="comment" rows="5" placeholder="@lang("
+                    messages.course.createReview.commentPlaceholder")" value="{{ old('comment') }}"></textarea>
+            </div>
+            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <input type="hidden" name="course_id" value="{{  $data['course']->getId() }}">
+            <button type="submit" class="btn btn-primary float-right">
+                @lang ("messages.course.create.submit")
+            </button>
+        </form>
+    </div>
+</div>
+@endif
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,5 +1,4 @@
 <!--Authors: Simon Florez Silva (sflorezs1@eafit.edu.co), Adrián Gutiérrez (aagutierrl@eafit.edu.co), Vincent Arcila (vaarcilal@eafit.edu.co)-->
-<!--Last edition: September 22 -->
 
 @extends('layouts.master')
 
@@ -10,7 +9,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @include('util.message')
+            @include ('util.message')
             <div class="card">
                 <div class="card-header d-flex justify-content-between align-content-center">
                     {{ $data["course"]->getId() }} - {{ $data["course"]->getTitle() }}
@@ -117,41 +116,7 @@
                 </div>
             </div>
         </div>
-        @if ( $data['isEnrolled'] )
-        <div class="card">
-            <div class="card-header">@lang("messages.course.createReview.cardTitle")</div>
-            <div class="card-body">
-                @if($errors->any())
-                <div class="alert alert-warning" role="alert">
-                    <ul id="errors">
-                        @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-                <form method="POST" action="{{ route('review.save') }}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="rating">@lang("messages.course.review.rating")</label>
-                        <input type="number" class="form-control" name="rating" min="0" max="10" placeholder=10
-                            value="{{ old('title') }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="comment">@lang("messages.course.review.comment")</label>
-                        <textarea class="form-control" name="comment" rows="5" placeholder="@lang("
-                            messages.course.createReview.commentPlaceholder")" value="{{ old('comment') }}">
-                            </textarea>
-                    </div>
-                    <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                    <input type="hidden" name="course_id" value="{{  $data['course']->getId() }}">
-                    <button type="submit" class="btn btn-primary float-right">
-                        @lang("messages.course.create.submit")
-                    </button>
-                </form>
-            </div>
-        </div>
-        @endif
+        @include ('courses.createReview')
     </div>
 </div>
 </div>
