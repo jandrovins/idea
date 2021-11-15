41a1ff3bd945e2a9a5653a9b4a0e050e105d9235
Vincent A. Arcila <jandro.vins@gmail.com>
2021-11-15 23:54:54 +0000

    Fix bug

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -30,7 +30,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    @lang('messages.confirm_password')
+                                    @lang('messages.auth.confirm_password')
                                 </button>
 
                                 @if (Route::has('password.request'))
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -42,7 +42,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">@lang('messages.confirm_password')</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">@lang('messages.auth.confirm_password')</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
