9ca767e0017bc3e7b0052571a810753e36eca77e
sflorezs1 <53716139+sflorezs1@users.noreply.github.com>
2021-11-15 20:29:03 -0500

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -31,3 +31,29 @@ function moveFooter() {
     }
 }
 
+// Dom observer, looks for changes in attributes in given element and executes a callback
+var observeDOM = (function(){
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return function( obj, callback ){
+    if( !obj || obj.nodeType !== 1 ) return; 
+
+    if( MutationObserver ){
+      // define a new observer
+      var mutationObserver = new MutationObserver(callback);
+
+      // have the observer observe foo for changes in children
+      mutationObserver.observe( obj, { attributes: true });
+      return mutationObserver;
+    }
+    
+    // browser support fallback
+    else if( window.addEventListener ){
+      obj.addEventListener('DOMNodeInserted', callback, false)
+      obj.addEventListener('DOMNodeRemoved', callback, false)
+    }
+  }
+})();
+
+// Move footer on details open
+observeDOM(document.querySelector('details.details-example'), moveFooter);
